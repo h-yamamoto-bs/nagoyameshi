bce39b907fe125bf8cf787609f96f12521248e05
h.yamamoto.bs <h.yamamoto.bs@gmail.com>
2025-07-02 18:08:24 +0900

    shopsのmodels.py作成

--- nagoyameshi/shops/models.py
+++ nagoyameshi/shops/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from accounts.models import User
 
 # Create your models here.
+class Shop(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+    address = models.CharField(max_length=255, null=False, blank=False)
+    seat_count = models.PositiveIntegerField(null=False, blank=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
+
+    class Meta:
+        db_table = 'shops'
+
+    def __str__(self):
+        return f"{self.name}（{self.address}）"
