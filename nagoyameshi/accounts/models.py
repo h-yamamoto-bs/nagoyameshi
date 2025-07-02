5fb4b9bcb691a05178562a279237afb5dfc85c8b
h.yamamoto.bs <h.yamamoto.bs@gmail.com>
2025-07-02 18:06:40 +0900

    accountsのmodels.py作成

--- nagoyameshi/accounts/models.py
+++ nagoyameshi/accounts/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 # Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, mail, password=None, **extra_fields):
+        if not mail:
+            raise ValueError('メールアドレスは必須です')
+        user = self.model(mail=self.normalize_email(mail), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, mail, password=None, **extra_fields):
+        extra_fields.setdefault('manager_flag', True)
+        user = self.create_user(mail, password, **extra_fields)
+        user.is_superuser = True
+        user.is_staff = True
+        user.is_active = True
+        user.save(using=self._db)
+        return user
+
+class User(AbstractBaseUser):
+    mail = models.EmailField(unique=True)
+    manager_flag = models.BooleanField(default=False)
+    job = models.CharField(max_length=100, null=True, blank=True)
+    birth_year = models.PositiveIntegerField(null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'mail'
+    objects = UserManager()
+
+    def __str__(self):
+        return f"{self.mail} ({'管理者' if self.manager_flag else '一般'})"
+
+    def has_perm(self, perm, obj=None):
+        return self.is_superuser
+
+    def has_module_perms(self, app_label):
+        return self.is_superuser
+
+    class Meta:
+        db_table = 'auth_user'
