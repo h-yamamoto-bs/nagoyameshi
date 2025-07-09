c022edfaf31ad52e9da8e0e2e0ba4962af9be74a
h.yamamoto.bs <h.yamamoto.bs@gmail.com>
2025-07-09 17:42:53 +0900

    Shopモデルに電話番号を追加

--- shops/models.py
+++ shops/models.py
@@ -6,6 +6,7 @@ class Shop(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False)
     address = models.CharField(max_length=255, null=False, blank=False)
     seat_count = models.PositiveIntegerField(null=False, blank=False)
+    phone_number = models.CharField(max_length=15, null=True, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
 
     class Meta:
