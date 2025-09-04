ffcf285b2bd7660cd4797b85c74b93046aa2e5e9
h.yamamoto.bs <h.yamamoto.bs@gmail.com>
2025-09-04 16:50:42 +0900

    Add home redirect to shops:shop_list

--- nagoyameshi/urls.py
+++ nagoyameshi/urls.py
@@ -19,8 +19,10 @@ from django.urls import path
 from django.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda request: redirect('shops:shop_list'), name='home'),  # ルートを shops:shop_list にリダイレクト
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('shops/', include('shops.urls')),
